2269edc89bc770504043c3297befcbf5a22260b5
Tate Keller <tatekeller@Tates-MacBook-Pro.local>
2017-09-15 17:44:46 -0400

    Plot Variables Script Updated - Clean, Labelled Graphs

--- PlotVariables.py
+++ PlotVariables.py
@@ -3,6 +3,10 @@ import csv
 import matplotlib
 matplotlib.use('Agg') # force matplotlib to not use XWindows backend
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.gridspec as gridspec
+#import plotly.plotly as py
+#import plotly.tools as tls
 
 #os.chdir("/Users/tatekeller/Documents/Aplysia Feeding Kinetic Model/AplysiaFeedingKineticModel")
 #The following code reads the SlugOutput2.txt file and converts it to a csv file
@@ -138,64 +142,110 @@ with open("SlugOutput2-csvTranslated.csv", "r") as file:
 #print(freqI1I3)
 
 #The following code plots the entire figure
-plt.figure(1)
+figure = plt.figure(figsize = (18,18))
+timeticks = np.arange(0,9,1)
+#The figure is a Grid with 5 rows, 2 columns. The input graphs are in the left column, output in the right
+gs = gridspec.GridSpec(5, 2)
 
 #The following code subplots the position vs time graph
-plt.subplot(811)
-plt.plot(time[1:850],position[1:850])
-plt.xlabel(time[0])
-plt.ylabel(position[0])
-plt.axis([0, 9, -0.006, 0.004])
-plt.grid(True)
+positiongraph = figure.add_subplot(gs[0,1])
+positionticks = np.arange(-.006,.004, .001)
+positiongraph.plot(time[1:850],position[1:850])
+positiongraph.set_title('Odontophore Position')
+positiongraph.set_xlabel(time[0] + ' (seconds)')
+positiongraph.set_ylabel(position[0] + ' (Meters)')
+positiongraph.axis([0, 9, -0.006, 0.004])
+positiongraph.set_xticks(timeticks)
+positiongraph.set_yticks(positionticks)
+positiongraph.grid(True)
+
 #The following code subplots the Odontophore major axis vs time graph
-plt.subplot(812)
-plt.plot(time[1:850],radius[1:850])
-plt.xlabel(time[0])
-plt.ylabel(radius[0])
-plt.axis([0, 9, 0, 0.009])
-plt.grid(True)
+radiusgraph = figure.add_subplot(gs[1,1])
+radiusticks = np.arange(0,.009, .001)
+radiusgraph.plot(time[1:850],radius[1:850])
+radiusgraph.set_title('Odontophore Major Axis')
+radiusgraph.set_xlabel(time[0] + ' (seconds)')
+radiusgraph.set_ylabel(radius[0] + ' (Meters)')
+radiusgraph.axis([0, 9, 0, 0.009])
+radiusgraph.set_xticks(timeticks)
+radiusgraph.set_yticks(radiusticks)
+radiusgraph.grid(True)
+
 #The following code subplots the Angle vs time graph
-plt.subplot(813)
-plt.plot(time[1:850],angle[1:850])
-plt.xlabel(time[0])
-plt.ylabel(angle[0])
-plt.axis([0, 9, -10, 100])
-plt.grid(True)
+anglegraph = figure.add_subplot(gs[2,1])
+angleticks = np.arange(-10,100, 10)
+anglegraph.plot(time[1:850],angle[1:850])
+anglegraph.set_title('Odontophore Angle')
+anglegraph.set_xlabel(time[0] + ' (seconds)')
+anglegraph.set_ylabel(angle[0] + ' (Degrees)')
+anglegraph.axis([0, 9, -10, 100])
+anglegraph.set_xticks(timeticks)
+anglegraph.set_yticks(angleticks)
+anglegraph.grid(True)
+
 #The following code subplots the Seaweed Force vs time graph
-plt.subplot(814)
-plt.plot(time[1:850],seaweedforce[1:850])
-plt.xlabel(time[0])
-plt.ylabel(seaweedforce[0])
-plt.axis([0, 9, -20, 120])
-plt.grid(True)
+forcegraph = figure.add_subplot(gs[0,0])
+forceticks = np.arange(-20,120, 20)
+forcegraph.plot(time[1:850],seaweedforce[1:850])
+forcegraph.set_title('Force From Seaweed')
+forcegraph.set_xlabel(time[0] + ' (seconds)')
+forcegraph.set_ylabel(seaweedforce[0] + ' (mN)')
+forcegraph.axis([0, 9, -20, 120])
+forcegraph.set_xticks(timeticks)
+forcegraph.set_yticks(forceticks)
+forcegraph.grid(True)
+
 #The following code subplots the Odontophore input vs time graph
-plt.subplot(815)
-plt.plot(time[1:850],freqN3[1:850])
-plt.xlabel(time[0])
-plt.ylabel(freqN3[0])
-plt.axis([0, 9, -5, 35])
-plt.grid(True)
+n3graph = figure.add_subplot(gs[1,0])
+n3ticks = np.arange(-5,35, 5)
+n3graph.plot(time[1:850],freqN3[1:850])
+n3graph.set_title('Odontophore Input')
+n3graph.set_xlabel(time[0] + ' (seconds)')
+n3graph.set_ylabel(freqN3[0] + ' (Hz)')
+n3graph.axis([0, 9, -5, 35])
+n3graph.set_xticks(timeticks)
+n3graph.set_yticks(n3ticks)
+n3graph.grid(True)
+
 #The following code subplots the Hinge input vs time graph
-plt.subplot(816)
-plt.plot(time[1:850],freqHinge[1:850])
-plt.xlabel(time[0])
-plt.ylabel(freqHinge[0])
-plt.axis([0, 9, -5, 25])
-plt.grid(True)
+hingegraph = figure.add_subplot(gs[2,0])
+hingeticks = np.arange(-5,25, 5)
+hingegraph.plot(time[1:850],freqHinge[1:850])
+hingegraph.set_title('Hinge Input')
+hingegraph.set_xlabel(time[0] + ' (seconds)')
+hingegraph.set_ylabel(freqHinge[0] + ' (Hz)')
+hingegraph.axis([0, 9, -5, 25])
+hingegraph.set_xticks(timeticks)
+hingegraph.set_yticks(hingeticks)
+hingegraph.grid(True)
+
 #The following code subplots the I2 input vs time graph
-plt.subplot(817)
-plt.plot(time[1:850],freqI2[1:850])
-plt.xlabel(time[0])
-plt.ylabel(freqI2[0])
-plt.axis([0, 9, -5, 25])
-plt.grid(True)
+i2graph = figure.add_subplot(gs[3,0])
+i2ticks = np.arange(-5,25,5)
+i2graph.plot(time[1:850],freqI2[1:850])
+i2graph.set_title('I2 Input')
+i2graph.set_xlabel(time[0] + ' (seconds)')
+i2graph.set_ylabel(freqI2[0] + ' (Hz)')
+i2graph.axis([0, 9, -5, 25])
+i2graph.set_xticks(timeticks)
+i2graph.set_yticks(i2ticks)
+i2graph.grid(True)
+
 #The following code subplots the I1/I3 vs time graph
-plt.subplot(818)
-plt.plot(time[1:850],freqI1I3[1:850])
-plt.xlabel(time[0])
-plt.ylabel(freqI1I3[0])
-plt.axis([0, 9, -5, 25])
-plt.grid(True)
+i1i3graph = figure.add_subplot(gs[4,0])
+i1i3ticks = np.arange(-5,25,5)
+i1i3graph.plot(time[1:850],freqI1I3[1:850])
+i1i3graph.set_title('I1/I3 Input')
+i1i3graph.set_xlabel(time[0] + ' (seconds)')
+i1i3graph.set_ylabel(freqI1I3[0] + ' (Hz)')
+i1i3graph.axis([0, 9, -5, 25])
+i1i3graph.set_xticks(timeticks)
+i1i3graph.set_yticks(i1i3ticks)
+i1i3graph.grid(True)
+
 #Show the entire figure
 #plt.show()
-plt.savefig("plot.pdf", bbox_inches='tight')
+
+#Save Figure to PDF
+figure.tight_layout()
+figure.savefig("plot.pdf")
